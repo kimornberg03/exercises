feb4b0ed03c2f1926a3a87b7011353860540c8a3
unknown <kim.ornberg@skola.botkyrka.se>
2020-10-13 20:27:51 +0200

    twentythree

--- A-Beginner/23/script.js
+++ A-Beginner/23/script.js
@@ -1,11 +1,25 @@
-console.log(isDivisibleByFive(25))
+console.log(isDivisibleByFive2(25))
 
 
 
-function isDivisibleByFive(number) {
+function isDivisibleByFive1(number) {
 
 
     if ((number % 5) == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function isDivisibleByFive2(number) {
+    let x = number / 5
+    let y = Math.floor(x)
+
+    let z = x - y
+
+
+    if (z == 0) {
         return true
     } else {
         return false
