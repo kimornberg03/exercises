625ff9fc2bc4e1e398b8c53668b3e2ba09de7e3b
unknown <kim.ornberg@skola.botkyrka.se>
2020-10-13 20:09:25 +0200

    eighteen

---
+++ A-Beginner/18/script.js
@@ -0,0 +1,17 @@
+let x = "Hej"
+let y = "Hej"
+
+
+console.log(Iss(x, y))
+
+
+function Iss(str1, str2) {
+
+
+    if (str1 == str2) {
+        return true
+    } else {
+        return false
+    }
+
+}
