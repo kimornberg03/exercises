2b0f95932167e22dda1c55f64405ffef34ae9cd0
unknown <kim.ornberg@skola.botkyrka.se>
2020-10-13 19:52:00 +0200

    sixteen

---
+++ A-Beginner/16/script.js
@@ -0,0 +1,17 @@
+let x = 75
+
+
+console.log(CheckAge(x))
+
+
+
+
+function CheckAge(Age) {
+
+    if (Age < 65) {
+        return "young"
+    } else {
+        return "old"
+    }
+
+}
---
+++ A-Beginner/17/script.js
@@ -0,0 +1,10 @@
+function IsTanja() {
+
+}
+if (IsTanja = "Tanja")
+
+    console.log("True")
+
+else
+
+    console.log("False")
