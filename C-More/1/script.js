604054a0dea7b82315fc6942b4444f8ddd254d7a
unknown <kim.ornberg@skola.botkyrka.se>
2020-10-16 18:56:47 +0200

    one

---
+++ C-More/1/script.js
@@ -0,0 +1,8 @@
+let capital = ["Tokyo", "Jakarta", "Paris", "Santiago", "Nairobi"];
+
+console.log(Lastelement(capital))
+
+function Lastelement(xarray) {
+    let y = (xarray.length - 1);
+    return xarray[y]
+}
