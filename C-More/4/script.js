2ea10d4eb73c1245651874655d6db2c2a5728ffd
unknown <kim.ornberg@skola.botkyrka.se>
2020-10-18 12:46:52 +0200

    four

--- C-More/4/script.js
+++ C-More/4/script.js
@@ -1,11 +1,11 @@
 let firstperson = { Name: "Kim", LastName: "Örnberg", Age: "16", Email: "Kim.ornberg@gmail.com" }
 let secondperson = { Name: "Karl", Lastname: "svensson", Age: "16", Email: "Karl.Svenson@gmail.com" }
 
-console.log(isSameUser(firstperson, secondperson))
+console.log(isSameUser(firstperson.Email, secondperson.Email))
 
 function isSameUser(x, y) {
 
-    if (x.Email == y.Email) {
+    if (x == y) {
         return true
     } else {
         return false
