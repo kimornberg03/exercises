77ef3c75253f637fb846d5574b7a28e5dfe4c2e0
unknown <kim.ornberg@skola.botkyrka.se>
2020-10-18 14:59:00 +0200

    eight

--- C-More/8/script.js
+++ C-More/8/script.js
@@ -1,11 +1,20 @@
-let object = { Name: "Kim", Lastname: "Örnberg", Age: "17" }
+let ob1 = { Name: "Kim", Lastname: "Örnberg", Age: "17" }
+let ob2 = { Name: "Ulf", Lastname: "Örnberg", Age: "56" }
+let persArray = []
 
-console.log(Fullname(object))
+persArray.push(ob1)
+persArray.push(ob2)
+
+
+console.log(Fullname(persArray))
 
 function Fullname(x) {
 
-    let y = x.Name + " " + x.Lastname
+    let retarray = [];
 
-    return y
+    for (let index = 0; index < x.length; index++) {
+        retarray.push(x[index].Name + " " + x[index].Lastname)
 
+    }
+    return retarray
 }
